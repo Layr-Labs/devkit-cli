1523ec5d65f2bff347ffc44dd91ff27f6b6ef2ff
supernova <supernova@onlyevm.com>
2025-04-24 13:17:15 +0530

    fix test

--- pkg/common/config_test.go
+++ pkg/common/config_test.go
@@ -72,6 +72,8 @@ func TestLoadEigenConfig_FromCopiedTempFile(t *testing.T) {
 	// Release
 	assert.Equal(t, "some-org/avs-logic:v0.1", cfg.Release.AVSLogicImageTag)
 	assert.False(t, cfg.Release.PushImage)
+	// log
+	assert.Equal(t, "debug", cfg.Log.Level)
 }
 
 func LoadEigenConfigFromPath(path string) (*common.EigenConfig, error) {
