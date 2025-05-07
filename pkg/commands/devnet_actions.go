bbfd48ca5bb6c160c239f409806c41bd1e521ee8
supernova <supernova@onlyevm.com>
2025-05-07 16:25:29 +0530

    only have deploy command

--- pkg/commands/devnet_actions.go
+++ pkg/commands/devnet_actions.go
@@ -1,20 +1,17 @@
 package commands
 
 import (
-	"context"
 	"devkit-cli/pkg/common"
 	"devkit-cli/pkg/common/devnet"
 	"fmt"
 	"log"
 	"os"
 	"os/exec"
-	"strings"
 	"time"
 
-	"github.com/ethereum/go-ethereum"
-	"github.com/ethereum/go-ethereum/accounts/abi"
-	gethcommon "github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/ethclient"
+	// "github.com/ethereum/go-ethereum/accounts/abi"
+	// gethcommon "github.com/ethereum/go-ethereum/common"
+	// "github.com/ethereum/go-ethereum/ethclient"
 	"github.com/urfave/cli/v2"
 )
 
@@ -75,137 +72,50 @@ func StartDevnetAction(cCtx *cli.Context) error {
 
 
 
-	hourglass_cmd := exec.Command("make", "-f", common.DevkitMakefile, "deploy-taskmailbox")
+	hourglass_cmd := exec.Command("make", "-f", common.DevkitMakefile, "deploy")
 	hourglass_cmd.Stdout = os.Stdout
 	hourglass_cmd.Stderr = os.Stderr
 	if err := hourglass_cmd.Run(); err != nil {
 		return err
 	}
-	log.Printf("TaskMailbox contract deployed!")
+	
 
-	hourglass_cmd = exec.Command("make", "-f", common.DevkitMakefile, "deploy-taskavsregistrar")
-	hourglass_cmd.Stdout = os.Stdout
-	hourglass_cmd.Stderr = os.Stderr
-	if err := hourglass_cmd.Run(); err != nil {
-		return err
-	}
+	
 
-	log.Printf("TaskAvsRegistrar contract deployed!")
-
-	hourglass_cmd = exec.Command("make", "-f", common.DevkitMakefile, "deploy-avsl2contracts")
-	hourglass_cmd.Stdout = os.Stdout
-	hourglass_cmd.Stderr = os.Stderr
-	if err := hourglass_cmd.Run(); err != nil {
-		return err
-	}
-
-	log.Printf("Avs L2 contracts deployed!")
-
-	hourglass_cmd = exec.Command("make", "-f", common.DevkitMakefile, "setup-avsl1contracts")
-	hourglass_cmd.Stdout = os.Stdout
-	hourglass_cmd.Stderr = os.Stderr
-	if err := hourglass_cmd.Run(); err != nil {
-		return err
-	}
-
-	log.Printf("Avs L1 contracts setup success!")
-
-	hourglass_cmd = exec.Command("make", "-f", common.DevkitMakefile, "create-operator-set")
-	hourglass_cmd.Stdout = os.Stdout
-	hourglass_cmd.Stderr = os.Stderr
-	if err := hourglass_cmd.Run(); err != nil {
-		return err
-	}
-
-	log.Printf("Created operator sets")
-
-	hourglass_cmd = exec.Command("make", "-f", common.DevkitMakefile, "setup-avstaskmailbox-config")
-	hourglass_cmd.Stdout = os.Stdout
-	hourglass_cmd.Stderr = os.Stderr
-	if err := hourglass_cmd.Run(); err != nil {
-		return err
-	}
-
-	log.Printf("Avs task mailbox setup complete")
-
-
-	hourglass_cmd = exec.Command("make", "-f", common.DevkitMakefile, "register-operator-to-eigenlayer")
-	hourglass_cmd.Stdout = os.Stdout
-	hourglass_cmd.Stderr = os.Stderr
-	if err := hourglass_cmd.Run(); err != nil {
-		return err
-	}
-
-	log.Printf("Registered operator to eigenlayer")
-
-	contractAddr := gethcommon.HexToAddress(devnet.CONTRACTS_REGISTRY)
-	client, err := ethclient.Dial(rpc_url)
-	if err != nil {
-		log.Fatalf("Failed to connect to RPC: %v", err)
-	}
-
-	parsedABI, err := abi.JSON(strings.NewReader(devnet.CONTRACTS_REGISTRY_ABI))
-	if err != nil {
-		log.Fatalf("Failed to parse ABI: %v", err)
-	}
-
-	input, err := parsedABI.Pack("nameToAddress", "TaskAVSRegistrar")
-	if err != nil {
-		log.Fatalf("Failed to pack input: %v", err)
-	}
-
-	msg := ethereum.CallMsg{
-		To:   &contractAddr,
-		Data: input,
-	}
-
-	output, err := client.CallContract(context.Background(), msg, nil)
-	if err != nil {
-		log.Fatalf("Failed to call contract: %v", err)
-	}
-
-	var result gethcommon.Address
-	if err := parsedABI.UnpackIntoInterface(&result, "nameToAddress", output); err != nil {
-		log.Fatalf("Failed to unpack output: %v", err)
-	}
-
-	fmt.Printf("TaskAVSRegistrar address: %s\n", result.Hex())
-
-
-	hourglassCmd := exec.Command("make", "-f", common.DevkitMakefile, "register-operator-to-avs")
-	hourglassCmd.Stdout = os.Stdout
-	hourglassCmd.Stderr = os.Stderr
-
-	// Convert to strings and pass as environment variables
-	hourglassCmd.Env = append(os.Environ(),
-		fmt.Sprintf("RPC_URL=%s", rpc_url),
-		fmt.Sprintf("REGISTRAR_ADDRESS=%s", result.Hex()),
-	)
-
-	if err := hourglassCmd.Run(); err != nil {
-		return err
-	}
-	log.Printf("✅ Registered operator to AVS")
-
-
-	// hourglassCmd = exec.Command("make", "-f", common.DevkitMakefile, "deposit-into-strategies")
-	// hourglassCmd.Stdout = os.Stdout
-	// hourglassCmd.Stderr = os.Stderr
-
-	// if err := hourglassCmd.Run(); err != nil {
-	// 	return err
+	// contractAddr := gethcommon.HexToAddress(devnet.CONTRACTS_REGISTRY)
+	// client, err := ethclient.Dial(rpc_url)
+	// if err != nil {
+	// 	log.Fatalf("Failed to connect to RPC: %v", err)
 	// }
-	// log.Printf("✅ Deposited into strategies")
 
-
-	// hourglassCmd = exec.Command("make", "-f", common.DevkitMakefile, "modify-allocations")
-	// hourglassCmd.Stdout = os.Stdout
-	// hourglassCmd.Stderr = os.Stderr
-
-	// if err := hourglassCmd.Run(); err != nil {
-	// 	return err
+	// parsedABI, err := abi.JSON(strings.NewReader(devnet.CONTRACTS_REGISTRY_ABI))
+	// if err != nil {
+	// 	log.Fatalf("Failed to parse ABI: %v", err)
 	// }
-	// log.Printf("✅ Modify allocations")
+
+	// input, err := parsedABI.Pack("nameToAddress", "TaskAVSRegistrar")
+	// if err != nil {
+	// 	log.Fatalf("Failed to pack input: %v", err)
+	// }
+
+	// msg := ethereum.CallMsg{
+	// 	To:   &contractAddr,
+	// 	Data: input,
+	// }
+
+	// output, err := client.CallContract(context.Background(), msg, nil)
+	// if err != nil {
+	// 	log.Fatalf("Failed to call contract: %v", err)
+	// }
+
+	// var result gethcommon.Address
+	// if err := parsedABI.UnpackIntoInterface(&result, "nameToAddress", output); err != nil {
+	// 	log.Fatalf("Failed to unpack output: %v", err)
+	// }
+
+	// fmt.Printf("TaskAVSRegistrar address: %s\n", result.Hex())
+
+
 	
 	return nil
 }
