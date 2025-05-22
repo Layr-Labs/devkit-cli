7e0d51e02361a3a154d44c8a770151b60fb69dd1
Sean McGary <sean@seanmcgary.com>
2025-05-22 12:29:59 -0500

    update template to v0.0.8

--- pkg/commands/create_test.go
+++ pkg/commands/create_test.go
@@ -93,7 +93,7 @@ func TestCreateCommand(t *testing.T) {
 		}
 
 		// Create config.yaml
-		return copyDefaultConfigToProject(targetDir, projectName, "https://github.com/Layr-Labs/hourglass-avs-template", "v0.0.7", false)
+		return copyDefaultConfigToProject(targetDir, projectName, "https://github.com/Layr-Labs/hourglass-avs-template", "v0.0.8", false)
 	}
 
 	app := &cli.App{
