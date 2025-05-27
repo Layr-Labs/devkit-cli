f3f0eafe67ca7d09871f6e3142e6a5edd01a843f
supernova <supernova@onlyevm.com>
2025-05-27 16:53:35 +0530

    delete unused

--- pkg/common/devnet/constants.go
+++ pkg/common/devnet/constants.go
@@ -1,11 +1,5 @@
 package devnet
 
-import (
-	"fmt"
-	"os"
-	"runtime"
-)
-
 // Foundry Image Date : 21 April 2025
 const FOUNDRY_IMAGE = "ghcr.io/foundry-rs/foundry:stable"
 const CHAIN_ARGS = "--chain-id 31337"
@@ -16,14 +10,3 @@ const L1 = "l1"
 // @TODO: Add core eigenlayer deployment addresses to context
 const ALLOCATION_MANAGER_ADDRESS = "0x948a420b8CC1d6BFd0B6087C2E7c344a2CD0bc39"
 const DELEGATION_MANAGER_ADDRESS = "0x39053D51B77DC0d36036Fc1fCc8Cb819df8Ef37A"
-
-// GetDefaultRPCURL returns the default RPC URL with platform-aware host
-func GetDefaultRPCURL() string {
-	host := "localhost"
-	if dockersHost := os.Getenv("DOCKERS_HOST"); dockersHost != "" {
-		host = dockersHost
-	} else if runtime.GOOS != "linux" {
-		host = "host.docker.internal"
-	}
-	return fmt.Sprintf("http://%s:8545", host)
-}
